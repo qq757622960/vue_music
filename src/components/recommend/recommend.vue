d24ac59ea31305c4b9aad462e60ae10f78f16cf1
qq757622960 <qq757622960>
2018-11-10 18:35:47 +0800

    调用推荐接口
    调用推荐接口

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="recomment">
+        recomment
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import {getRecommend} from 'api/recommend'
+    import {ERR_OK} from 'api/config'
+
+    export default {
+        created() {
+            this._getRecommend()
+        },
+        methods: {
+            _getRecommend() {
+                getRecommend().then((res) => {
+                    if (res.code === ERR_OK) {
+                        console.log(res.data.slider);
+                    }
+                })
+            }
+        }
+    };
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    
+</style>
